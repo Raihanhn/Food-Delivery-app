b28c447e87d2e9a63de270b28e3c6dc8dd9b3056
raihan <raihanhn20@gmail.com>
2024-04-04 22:46:27 +0600

    App Download component added

---
+++ src/components/AppDownload/AppDownload.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./appdownload.css";
+
+const AppDownload = () => {
+  return (
+    <div className="app-download" id="app-download">
+      <p>
+        For Better Experience Dowload <br />
+        Tomato App{" "}
+      </p>
+    </div>
+  );
+};
+
+export default AppDownload;
