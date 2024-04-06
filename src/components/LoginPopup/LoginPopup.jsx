cef7d144c20051e4d6bea71fb0a9a714745b012f
raihan <raihanhn20@gmail.com>
2024-04-06 20:06:27 +0600

    signin signup style completed

--- src/components/LoginPopup/LoginPopup.jsx
+++ src/components/LoginPopup/LoginPopup.jsx
@@ -3,7 +3,7 @@ import "./loginpopup.css";
 import { assets } from "../../assets/assets";
 
 const LoginPopup = ({ setShowLogin }) => {
-  const [currentState, setCurrentState] = useState("Sign Up");
+  const [currentState, setCurrentState] = useState("Login");
   return (
     <div className="login-popup">
       <form className="login-popup-container">
@@ -16,11 +16,33 @@ const LoginPopup = ({ setShowLogin }) => {
           />
         </div>
         <div className="login-popup-inputs">
-          <input type="text" placeholder="Your name" required />
-          <input type="email" name="Your email" required />
+          {currentState === "Login" ? (
+            <></>
+          ) : (
+            <input type="text" placeholder="Your name" required />
+          )}
+
+          <input type="email" placeholder="Your email" required />
           <input type="password" placeholder="password" required />
         </div>
-        <button>{} </button>
+        <button>
+          {currentState === "Sign Up" ? "Create account" : "Login"}
+        </button>
+        <div className="login-popup-condition">
+          <input type="checkbox" required />
+          <p>By continuing, i agree the terms of use & privacy policy.</p>
+        </div>
+        {currentState === "Login" ? (
+          <p>
+            Create a new account?{" "}
+            <span onClick={() => setCurrentState("Sign Up")}>Click here</span>
+          </p>
+        ) : (
+          <p>
+            Already have an account?{" "}
+            <span onClick={() => setCurrentState("Login")}>Login here</span>
+          </p>
+        )}
       </form>
     </div>
   );
