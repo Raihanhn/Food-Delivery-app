88876e0687be3786cfe927c9a6f8a8689beb57b7
raihan <raihanhn20@gmail.com>
2024-04-08 00:18:45 +0600

    subtotal and total added

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -3,12 +3,13 @@ import "./cart.css";
 import { StoreContext } from "../../context/StoreContext";
 
 const Cart = () => {
-  const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
+  const { cartItems, food_list, removeFromCart, getTotalCartAmount } =
+    useContext(StoreContext);
 
   return (
     <div className="cart">
       <div className="cart-items">
-        <div className="cart-items title">
+        <div className="cart-items-title">
           <p>Items</p>
           <p>Title</p>
           <p>Price</p>
@@ -21,17 +22,53 @@ const Cart = () => {
         {food_list.map((item, index) => {
           if (cartItems[item._id] > 0) {
             return (
-              <div className="cart-items-title cart-items-item">
-                <img src={item.image} alt="" />
-                <p>{item.name} </p>
-                <p> {item.price} </p>
-                <p>{cartItems[item._id]} </p>
-                <p>{item.price * cartItems[item._id]} </p>
-                <p>x </p>
+              <div key={index}>
+                <div className="cart-items-title cart-items-item">
+                  <img src={item.image} alt="" />
+                  <p>{item.name} </p>
+                  <p>${item.price} </p>
+                  <p>{cartItems[item._id]} </p>
+                  <p>${item.price * cartItems[item._id]} </p>
+                  <p onClick={() => removeFromCart(item._id)} className="cross">
+                    x{" "}
+                  </p>
+                </div>
+                <hr />
               </div>
             );
           }
         })}
+      </div>
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Cart Total</h2>
+          <div className="">
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>{getTotalCartAmount()} </p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>{2} </p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>{getTotalCartAmount() + 2} </b>
+            </div>
+          </div>
+          <button>PROCEED TO CHECKOUT</button>
+        </div>
+        <div className="cart-promocode">
+          <div className="">
+            <p>If you have a promo code, Enter it here</p>
+            <div className="cart-promocode-input">
+              <input type="text" placeholder="promo code" />
+              <button>Submit</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
